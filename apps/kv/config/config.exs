243ef4ae6ff5a3aa7bbe78880059add26b1ab5f5
Marek Suscak <marek.suscak@gmail.com>
2016-08-25 03:23:01 +0200

    Add Router.

--- apps/kv/config/config.exs
+++ apps/kv/config/config.exs
@@ -2,6 +2,13 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
+# You can change default prompt too
+# config :iex, default_prompt: ">>>"
+
+config :kv, :routing_table,
+       [{?a..?m, :"foo@marek"},
+        {?n..?z, :"bar@marek"}]
+
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
